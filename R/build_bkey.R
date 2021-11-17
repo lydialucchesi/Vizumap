fc6d25d5c61858b6554e36caf2380df74a6c8bed
Lucchesi <Lydia.Lucchesi@anu.edu.au>
2021-11-18 10:53:15 +1100

    petra esa talk expert ranges addition

--- R/build_bkey.R
+++ R/build_bkey.R
@@ -1,22 +1,37 @@
 #'Build a bivariate colour key
 #'
-#'This function links data and a colour palette. Numerical bounds are added
-#'to the 3 x 3 colour grid.
+#'This function links data and a colour palette. Numerical bounds are added to
+#'the 3 x 3 colour grid.
 #'
 #'A key for the bivariate map is not automatically generated with
-#'\code{\link{build_bmap}} and must be made using \code{\link{build_bkey}}. It is important
-#'that the arguments passed to \code{\link{build_bkey}} match those passed to
-#'\code{\link{build_bmap}}. The map and key can be viewed together using
-#'\code{\link{attach_key}}.
+#'\code{\link{build_bmap}} and must be made using \code{\link{build_bkey}}. It
+#'is important that the arguments passed to \code{\link{build_bkey}} match those
+#'passed to \code{\link{build_bmap}}. The map and key can be viewed together
+#'using \code{\link{attach_key}}.
 #'
 #'@param data A data frame.
 #'@param palette Name of colour palette or character vector of hex colour codes
-#'  created with \code{\link{build_palette}} function. Colour palette names include
-#'  \code{BlueYellow}, \code{CyanMagenta}, \code{BlueRed} and
+#'  created with \code{\link{build_palette}} function. Colour palette names
+#'  include \code{BlueYellow}, \code{CyanMagenta}, \code{BlueRed} and
 #'  \code{GreenBlue}.
 #'@param terciles A logical value. This provides the option to define numerical
 #'  bounds for the colour key grid using terciles instead of equal intervals.
-#'@param flipAxis A logical value. Whether to place the axis on the opposite sides or not.
+#'@param flipAxis A logical value. Whether to place the axis on the opposite
+#'  sides or not.
+#'@param expertR_est A vector consisting of the range of expert values for the
+#'  estimate (Default: NA).
+#'@param expertR_err A vector consisting of the range of expert values for the
+#'  error (Default: NA).
+#'@param bound A vector of eight elements representing the bounds for the
+#'  estimate and error that will be used on the bivariate colour key.  These can
+#'  be created offline using the \code{\link{findNbounds}} function.
+#'
+#'@details If bound is NULL, the bounds for the legend will be computed from the
+#'  data and expert bounds (if available) using the findNbound function.  The
+#'  argument bound should only be used if you want to make comparison against
+#'  multiple maps.  For that scenario, the user should use the findNbounds
+#'  function to generate a bound for the larger set of data that wish to
+#'  compare.
 #'
 #'@seealso \code{\link{attach_key}}
 #'
@@ -37,68 +52,122 @@
 #'view(key)
 #'@export
 
-
-build_bkey <- function(data, palette = "BlueYellow", terciles = FALSE, flipAxis = FALSE) {
-
-  estimate <- names(data)[1]
-  error <- names(data)[2]
-
-  # define color ramps based on user input
-  if (class(palette)[1] == "character" & length(palette)==1) {
-    if (palette == "BlueYellow")
-      colors <- build_palette(name = "BlueYellow")
-   else if (palette == "CyanMagenta")
-     colors <- build_palette(name = "CyanMagenta")
-    else if (palette == "BlueRed")
-      colors <- build_palette(name = "BlueRed")
-    else if (palette == "GreenBlue")
-      colors <- build_palette(name = "GreenBlue")
+build_bkey <-
+  function (data,
+            palette = "BlueYellow",
+            terciles = FALSE,
+            flipAxis = FALSE,
+            expertR_est = NA,
+            expertR_err = NA,
+            bound = NULL)
+  {
+    estimate <- names(data)[1]
+    error <- names(data)[2]
+    if (class(palette)[1] == "character" & length(palette) ==
+        1) {
+      if (palette == "BlueYellow")
+        colors <- build_palette(name = "BlueYellow")
+      else if (palette == "CyanMagenta")
+        colors <- build_palette(name = "CyanMagenta")
+      else if (palette == "BlueRed")
+        colors <- build_palette(name = "BlueRed")
+      else if (palette == "GreenBlue")
+        colors <- build_palette(name = "GreenBlue")
+      else
+        stop(
+          "Palette name not recognised. Must be one of BlueYellow, CyanMagenta, BlueRed or GreenBlue.\n"
+        )
+    }
+    else if (class(palette)[1] == "palette")
+      colors <- palette
     else
-      stop("Palette name not recognised. Must be one of BlueYellow, CyanMagenta, BlueRed or GreenBlue.\n")
+      stop(
+        "Palette supplied is not of class 'palette'. Please create a palette using the 'build_palette' function."
+      )
+    if (!is.logical(flipAxis))
+      stop("flipAxis must be a logical value")
+    if (!is.logical(terciles))
+      stop("terciles must be a logical value")
+    x1 <- c(3, 4, 3, 2)
+    x2 <- c(4, 5, 4, 3)
+    x3 <- c(5, 6, 5, 4)
+    x <- c(x1, x1 - 1, x1 - 2, x2, x2 - 1, x2 - 2, x3, x3 - 1,
+           x3 - 2)
+    y1 <- c(0, 1, 2, 1)
+    y2 <- c(1, 2, 3, 2)
+    y3 <- c(2, 3, 4, 3)
+    y <- c(y1, y1 + 1, y1 + 2, y2, y2 + 1, y2 + 2, y3, y3 + 1,
+           y3 + 2)
+    group <- rep(1:9, each = 4)
+    clr <- rep(colors, each = 4)
+
+    tiles <- data.frame(
+      x = x,
+      y = y,
+      group = group,
+      color = clr
+    )
+    if (!flipAxis) {
+      if (is.null(bound)) {
+        bound <- findNbounds(
+          data = data,
+          estimate = estimate,
+          error = error,
+          terciles = terciles,
+          expertR_est = expertR_est,
+          expertR_err = expertR_err
+        )
+      }
+
+
+      labels <- data.frame(
+        x = c(2.5, 1.5, 0.5,-0.5, 3.5,
+              4.5, 5.5, 6.5),
+        y = c(-0.5, 0.5, 1.5, 2.5,-0.5,
+              0.5, 1.5, 2.5),
+        bound = as.character(bound),
+        angle = c(rep(45,
+                      4), rep(-45, 4))
+      )
+      p <- list(
+        tiles = tiles,
+        labels = labels,
+        estimate = estimate,
+        error = error,
+        flipped = flipAxis
+      )
+
+    }
+    else {
+      if (is.null(bound)) {
+        bound <-
+          findNbounds(
+            data = data,
+            estimate = error,
+            error = estimate,
+            terciles = terciles,
+            expertR_est = expertR_est,
+            expertR_err = expertR_err
+          )
+      }
+
+      labels <- data.frame(
+        y = c(2.5, 1.5, 0.5,-0.5, 3.5,
+              4.5, 5.5, 6.5),
+        x = c(-0.5, 0.5, 1.5, 2.5,-0.5,
+              0.5, 1.5, 2.5),
+        bound = as.character(bound),
+        angle = c(rep(45,
+                      4), rep(-45, 4))
+      )
+      p <- list(
+        tiles = tiles,
+        labels = labels,
+        estimate = error,
+        error = estimate,
+        flipped = flipAxis
+      )
+    }
+    oldClass(p) <- c("bivkey", class(p))
+    p
   }
-  else if (class(palette)[1] == "palette")
-    colors <- palette  # assign a users palette creation
-  else
-    stop("Palette supplied is not of class 'palette'. Please create a palette using the 'build_palette' function.")
-
-  if(!is.logical(flipAxis))
-    stop("flipAxis must be a logical value")
-
-  if(!is.logical(terciles))
-    stop("terciles must be a logical value")
-
-  x1 <- c(3, 4, 3, 2) ; x2 <- c(4, 5, 4, 3) ; x3 <- c(5, 6, 5, 4)
-  x <- c(x1, x1-1, x1-2, x2, x2-1, x2-2, x3, x3-1, x3-2)
-
-  y1 <- c(0, 1, 2, 1) ; y2 <- c(1, 2, 3, 2) ; y3 <- c(2, 3, 4, 3)
-  y <- c(y1, y1+1, y1+2, y2, y2+1, y2+2, y3, y3+1, y3+2)
-
-  group <- rep(1:9, each = 4)
-
-  clr <- rep(colors, each = 4)
-
-  tiles <- data.frame(x = x, y = y, group = group, color = clr)
-
-  if(!flipAxis) {
-    bound <- findNbounds(data = data, estimate = estimate, error = error, terciles = terciles)
-    labels <- data.frame(x = c(2.5, 1.5, .5, -.5, 3.5, 4.5, 5.5, 6.5),
-                         y = c(-.5, .5, 1.5, 2.5, -.5, .5, 1.5, 2.5),
-                         bound = as.character(bound), angle = c(rep(45,4), rep(-45, 4)))
-
-    p <- list(tiles = tiles, labels = labels, estimate = estimate, error = error, flipped = flipAxis)
-  } else {
-    bound <- findNbounds(data = data, estimate = error, error = estimate, terciles = terciles)
-    labels <- data.frame(y = c(2.5, 1.5, .5, -.5, 3.5, 4.5, 5.5, 6.5),
-                         x = c(-.5, .5, 1.5, 2.5, -.5, .5, 1.5, 2.5),
-                         bound = as.character(bound), angle = c(rep(45,4), rep(-45, 4)))
-
-    p <- list(tiles = tiles, labels = labels, estimate = error, error = estimate, flipped = flipAxis)
-  }
-
-  oldClass(p) <- c("bivkey", class(p))
-
-  p
-
-}
-
-
--- R/findNbounds.r
+++ R/findNbounds.r
@@ -2,45 +2,105 @@
 #'
 #' Find the bins for the colour grid
 #'
-#' @param data dataset from which bounds are to be determined
-#' @param estimate name of the estimate column
-#' @param error name of the error column
-#' @param terciles should terciles be calculated? (Default: FALSE)
+#' @param data Dataset from which bounds are to be determined.
+#' @param estimate Name of the estimate column.
+#' @param error Name of the error column.
+#' @param terciles Should terciles be calculated? (Default: FALSE).
+#' @param expertR_est A vector consisting of the range of expert values for the
+#'   estimate (Default: NA).
+#' @param expertR_err A vector consisting of the range of expert values for the
+#'   error (Default: NA).
+#'
+#' @details Expert ranges for the estimate and error can be supplied to assist
+#'   with the comparison of multiple figures. By default these are set to NA,
+#'   which will result in the bounds being directly computed from the data.
+#'   Note, if the expert values do not span the range of the data, this function
+#'   will default to finding the bounds of the data.
 #'
 #' @export
-findNbounds <- function(data, estimate, error, terciles){
 
+findNbounds <-
+  function (data,
+            estimate,
+            error,
+            terciles,
+            expertR_est = NA,
+            expertR_err = NA)
+  {
+    max_estimate <- max(c(expertR_est, data[, estimate]), na.rm = TRUE)
+    min_estimate <-
+      min(c(expertR_est, data[, estimate]), na.rm = TRUE)
+    max_error <- max(c(expertR_err, data[, error]), na.rm = TRUE)
+    min_error <- min(c(expertR_err, data[, error]), na.rm = TRUE)
+    width_estimate <- (max_estimate - min_estimate) / 3
+    width_error <- (max_error - min_error) / 3
 
-  # max and mins
-  max_estimate <- max(data[ ,estimate])
-  min_estimate <- min(data[ ,estimate])
-
-  max_error <- max(data[ ,error])
-  min_error <- min(data[ ,error])
-
-  # find width of a color bin for equal interval option
-  width_estimate <- (max_estimate  - min_estimate) / 3
-  width_error <- (max_error  - min_error) / 3
-
-  # find numerical bounds
-  estimate_bin1 <- calcbin(terciles = terciles, data = data, x = estimate, q = 1/3, bin = 1, width = width_estimate,
-                          min = min_estimate)
-  estimate_bin2 <- calcbin(terciles = terciles, data = data, x = estimate, q = 2/3, bin = 2, width = width_estimate,
-                           min = min_estimate)
-  estimate_bin3 <- calcbin(terciles = terciles, data = data, x = estimate, q = 1, bin = 3, width = width_estimate,
-                           min = min_estimate)
-
-  error_bin1 <- calcbin(terciles = terciles, data = data, x = error, q = 1/3, bin = 1, width = width_error,
-                        min = min_error)
-  error_bin2 <- calcbin(terciles = terciles, data = data, x = error, q = 2/3, bin = 2, width = width_error,
-                        min = min_error)
-  error_bin3 <- calcbin(terciles = terciles, data = data, x = error, q = 1, bin = 3, width = width_error,
-                        min = min_error)
-
-
-  # creating a data frame of labels for the color key grid
-  bound <- c(round(min_estimate, 2), estimate_bin1, estimate_bin2, estimate_bin3,
-             round(min_error, 2), error_bin1, error_bin2, error_bin3)
-
-  bound
-}
+    estimate_bin1 <- calcbin(
+      terciles = terciles,
+      data = data,
+      x = estimate,
+      q = 1 / 3,
+      bin = 1,
+      width = width_estimate,
+      min = min_estimate
+    )
+    estimate_bin2 <- calcbin(
+      terciles = terciles,
+      data = data,
+      x = estimate,
+      q = 2 / 3,
+      bin = 2,
+      width = width_estimate,
+      min = min_estimate
+    )
+    estimate_bin3 <- calcbin(
+      terciles = terciles,
+      data = data,
+      x = estimate,
+      q = 1,
+      bin = 3,
+      width = width_estimate,
+      min = min_estimate
+    )
+    error_bin1 <-
+      calcbin(
+        terciles = terciles,
+        data = data,
+        x = error,
+        q = 1 / 3,
+        bin = 1,
+        width = width_error,
+        min = min_error
+      )
+    error_bin2 <-
+      calcbin(
+        terciles = terciles,
+        data = data,
+        x = error,
+        q = 2 / 3,
+        bin = 2,
+        width = width_error,
+        min = min_error
+      )
+    error_bin3 <-
+      calcbin(
+        terciles = terciles,
+        data = data,
+        x = error,
+        q = 1,
+        bin = 3,
+        width = width_error,
+        min = min_error
+      )
+    bound <- c(
+      round(min_estimate, 2),
+      estimate_bin1,
+      estimate_bin2,
+      estimate_bin3,
+      round(min_error, 2),
+      error_bin1,
+      error_bin2,
+      error_bin3
+    )
+    bound
+  }
